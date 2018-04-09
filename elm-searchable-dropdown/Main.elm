a7dc51a8b2a3f59f1e35169f469c33cedf32193a
John Schoeman <johnschoeman1617@gmail.com>
2018-04-08 21:50:36 -0400

    Add code for elm searchable dropdown workshop project
    This code follows the workshop tutorial of elm written by JoelQ
    https://github.com/JoelQ/example-elm-searchable-dropdown

---
+++ elm-searchable-dropdown/Main.elm
@@ -0,0 +1,147 @@
+module Main exposing (main)
+
+import Html exposing (..)
+import Html.Events exposing (onClick, onInput)
+import Html.Attributes exposing (id, class)
+import Dom
+import Task
+
+
+main =
+  Html.program
+    { init = ( initialModel, Cmd.none )
+    , view = view
+    , update = update
+    , subscriptions = (\_ -> Sub.none)
+    }
+
+
+type State
+  = Open
+  | Closed
+
+
+type alias Model =
+  { state : State
+  , values : List String
+  , selected : Maybe String
+  , query : String
+  }
+
+
+initialModel : Model
+initialModel =
+  { state = Closed
+  , values = fruits
+  , selected = Nothing
+  , query = ""
+  }
+
+
+fruits : List String
+fruits =
+  [ "bannanas"
+  , "apples"
+  , "oranges"
+  , "peaches"
+  , "strawberries"
+  , "nectarines"
+  , "melons"
+  , "raspberries"
+  , "blueberries"
+  , "guava"
+  , "plantain"
+  ]
+
+
+view : Model -> Html Msg
+view model =
+  case model.state of
+    Open ->
+      viewOpen model
+
+    Closed ->
+      viewClosed model
+
+
+viewOpen : Model -> Html Msg
+viewOpen model =
+  div [ class "dropdown" ]
+    [ dropdownHead model.selected CloseSelect
+    , dropdownBody model
+    ]
+
+
+viewClosed : Model -> Html Msg
+viewClosed model =
+  div [ class "dropdown" ]
+    [ dropdownHead model.selected OpenSelect
+    ]
+
+
+dropdownItem : String -> Html Msg
+dropdownItem value =
+  li [ onClick (ItemSelected value) ] [ text value ]
+
+
+dropdownHead : Maybe String -> Msg -> Html Msg
+dropdownHead selected msg =
+  case selected of
+    Nothing ->
+      p [ onClick msg ] [ text "Click to toggle" ]
+
+    Just value ->
+      p [ onClick msg ] [ text value ]
+
+
+dropdownBody : Model -> Html Msg
+dropdownBody model =
+  div [ class "dropdown-body" ]
+      [ input [ id "search-box", onInput SearchInput ] []
+      , ul [] (List.map dropdownItem (filteredValues model))
+      ]
+
+
+filteredValues : Model -> List String
+filteredValues model =
+  List.filter (\v -> matchQuery model.query v) model.values
+
+
+matchQuery : String -> String -> Bool
+matchQuery needle haystack =
+  String.contains (String.toLower needle) (String.toLower haystack)
+
+
+type Msg
+  = OpenSelect
+  | CloseSelect
+  | ItemSelected String
+  | SearchInput String
+  | Noop
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  case msg of
+    Noop ->
+      ( model, Cmd.none )
+
+    OpenSelect ->
+      ( { model | state = Open }, focus )
+
+    CloseSelect ->
+      ( { model | state = Closed }, Cmd.none )
+
+    ItemSelected value ->
+      ( { model | selected = Just value, state = Closed, query = "" }
+      , Cmd.none
+      )
+
+    SearchInput query ->
+      ( { model | query = query }, Cmd.none )
+
+
+focus : Cmd Msg
+focus =
+  Dom.focus "search-box"
+    |> Task.attempt (always Noop)
