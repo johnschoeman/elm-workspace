29affd3dbfa936c29661fc260b9626e4f85171e0
John Schoeman <johnschoeman1617@gmail.com>
2018-04-09 09:01:07 -0400

    Add messages with payload in basic app
    This is used to make a counter and uses pattern matching in the update
    function, which is a bit magical to me at this point.

--- elm-architecture/App.elm
+++ elm-architecture/App.elm
@@ -8,20 +8,19 @@ import Html.Events exposing (onClick)
 
 
 type alias Model =
-  Bool
+  Int
 
 
 init : ( Model, Cmd Msg )
 init =
-  ( False, Cmd.none )
+  ( 0, Cmd.none )
 
 
 -- MESSAGES
 
 
 type Msg
-  = Expand
-  | Collapse
+  = Increment Int
 
 
 -- VIEW
@@ -29,14 +28,10 @@ type Msg
 
 view : Model -> Html Msg
 view model =
-  if model then
     div []
-        [ button [ onClick Collapse ] [ text "Collapse" ]
-        , text "Widget"
+        [ button [ onClick (Increment 2) ] [ text "+" ]
+        , text (toString model)
         ]
-  else
-    div []
-        [ button [ onClick Expand ] [ text "Expand" ] ]
 
 
 -- UPDATE
@@ -45,11 +40,8 @@ view model =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
-    Expand ->
-      ( True, Cmd.none )
-
-    Collapse ->
-      ( False, Cmd.none )
+    Increment howMuch ->
+      ( model + howMuch, Cmd.none )
 
 
 -- SUBSCRIPTIONS
