15e53d7856c4ad6d5cdb92c055eb1cd9f62a02ec
John Schoeman <johnschoeman1617@gmail.com>
2018-04-16 09:10:49 -0400

    Add routeing

--- elm-tutorial-players-app/src/Main.elm
+++ elm-tutorial-players-app/src/Main.elm
@@ -1,16 +1,21 @@
 module Main exposing (..)
 
-import Html exposing (program)
 import Msgs exposing (Msg)
 import Models exposing (Model, initialModel)
 import Update exposing (update)
 import View exposing (view)
 import Commands exposing (fetchPlayers)
+import Navigation exposing (Location)
+import Routing
 
 
-init : ( Model, Cmd Msg )
-init =
-  ( initialModel, fetchPlayers )
+init : Location -> ( Model, Cmd Msg )
+init location =
+  let
+      currentRoute =
+        Routing.parseLocation location
+  in
+      ( initialModel currentRoute, fetchPlayers )
 
 
 subscriptions : Model -> Sub Msg
@@ -18,12 +23,9 @@ subscriptions model =
   Sub.none
 
 
--- MAIN
-
-
 main : Program Never Model Msg
 main =
-  program
+  Navigation.program Msgs.OnLocationChange
     { init = init
     , view = view
     , update = update
